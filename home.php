7583b4f25f6a80cc850c62908077032d1f6a0969
Reginaldo Junior <76regi@gmail.com>
2015-08-11 23:24:31 -0300

    loop post dinamico

--- home.php
+++ home.php
@@ -19,30 +19,9 @@ get_header();
 		<div class="home-wrapper <?php echo $masonry; ?>">
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<div class="container" style="
-    border-bottom: dashed 2px;
-">
-				<div class="row">
-					<div class="col-md-8">
-						<h3>
-							h3. Lorem ipsum dolor sit amet.
-						</h3>
-						<p>
-							Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
-						</p> 
-						<button type="button" class="btn btn-info btn-lg btn-block">
-							Default
-						</button>
-					</div>
-					<div class="col-md-4" style="padding: 15px;">
-						<img alt="Bootstrap Image Preview" src="http://lorempixel.com/356/259/" />
-					</div>
-				</div>
-			</div>
-
 			<?php
 				
-				// get_template_part( 'content', 'large' );	
+				get_template_part( 'content', 'large' );	
 
 			?>
 
@@ -60,6 +39,7 @@ get_header();
 
 
 <?php
+
 get_footer(); 
 
 ?>
